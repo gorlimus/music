03d9cf65c48fd138c5447df75b76dd52aeeb1aa7
Gorlimus <gorlimus@gmail.com>
2023-01-26 18:11:52 +0200

    started a new project with vue/vite/pinia/tailwind. add first components/basic blocks/style

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <app-header />
+  <!-- Introduction -->
+  <app-intro />
+  <!-- Main Content -->
+  <app-main />
+  <!-- Player -->
+  <app-player />
+  <!-- Auth Modal -->
+  <app-auth />
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+import AppIntro from "@/components/AppIntro.vue";
+import AppMain from "@/components/AppMain.vue";
+import AppPlayer from "@/components/AppPlayer.vue";
+import AppAuth from "@/components/AppAuth.vue";
+
+export default {
+  name: "app",
+  components: {
+    AppHeader,
+    AppIntro,
+    AppMain,
+    AppPlayer,
+    AppAuth,
+  },
+};
+</script>
